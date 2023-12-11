794776804d2ae91b5b57e7a0a0521ce793ec2609
Bhavya Kasera <bhavya.kasera@yale.edu>
2023-12-11 16:08:01 -0500

    added code used for testing the portrait evaluation model

--- shuttographer/portrait_evaluation/dataset.py
+++ shuttographer/portrait_evaluation/dataset.py
@@ -3,11 +3,14 @@ from torchvision.datasets import ImageFolder
 import torchvision.transforms as transforms
 import numpy as np
 from utils import *
+import cv2
+import os
 
 class PortraitDataset(Dataset):
 
     def __init__(self, ds, transform=None):
         self.ds = ds
+        print('ds = ', ds)
         self.transform = transform
 
     def __len__(self):
@@ -21,6 +24,27 @@ class PortraitDataset(Dataset):
         zeros[label] = 1
         label = zeros
         return img, label
+
+class TestDataset(Dataset):
+    def __init__(self, test_folder, transform=None):
+        self.ds = []
+        for label in os.listdir(test_folder):
+            for image in os.listdir(test_folder+'/'+label):
+                img = cv2.imread(test_folder+'/'+label+'/'+image)
+                self.ds.append((image, img, int(label)))
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.ds)
+
+    def __getitem__(self, idx):
+        image, img, label = self.ds[idx]
+        if self.transform:
+            img = self.transform(img)
+        zeros = np.zeros(9)
+        zeros[label] = 1
+        label = zeros
+        return image, img, label
         
 class DeviceDataLoader:
     def __init__(self, dl, device):
---
+++ shuttographer/portrait_evaluation/testing.py
@@ -0,0 +1,57 @@
+import torch
+import gdown
+import os
+import torchvision.transforms as transforms
+import cv2
+import pandas
+from torchvision.datasets import ImageFolder
+from torch.utils.data import Dataset, random_split, DataLoader
+from utils import *
+from model import *
+from dataset import *
+
+device = ('cpu')
+portrait_model = QualityClassifier().to(device)
+model_url = 'https://drive.google.com/uc?id=1oQKuKN5N33KfSMptlNuNHWqmkrub1SAs'
+out = gdown.download(model_url, os.path.join('.', 'portrait_evaluation_model.pth'))
+if out is None:
+    print('failed to download model from drive')
+portrait_model.load_state_dict(torch.load('portrait_evaluation_model.pth')['model_state_dict'])
+portrait_model.eval()
+transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+    ])
+
+
+def get_quality_score(image):
+    image = transform(image)
+    image = torch.unsqueeze(image, 0)
+    with torch.no_grad():
+        output = portrait_model(image.to(device))
+    return torch.max(output, 1)[1].item()
+
+if __name__=='__main__':
+    # data = pandas.DataFrame(columns = ['name', 'predicted_score', 'true_score'])
+    names = []
+    pred_scores = []
+    true_scores = []
+    total_correct = 0
+    within_1 = 0
+    batch_size = 64
+    test_dataset = TestDataset('/home/bk632/portrait_data/test', transform)
+    for i in range(len(test_dataset)):
+        image_name, img, label = test_dataset[i]
+        outputs = portrait_model(img.unsqueeze(0))
+        preds, correct = torch.max(outputs, 1)[1], torch.max(torch.from_numpy(label), 0)[1]
+        total_correct += (preds==correct).sum().item()
+        if abs(preds.item()-correct.item()) <= 1:
+            within_1 += 1
+        names.append(image_name)
+        pred_scores.append(preds.item())
+        true_scores.append(correct.item())
+    print("accuracy = ", total_correct/len(test_dataset))
+    print("accuracy with tolerance of +/- 1 = ", within_1/len(test_dataset))
+    data = pandas.DataFrame({'name':names, 'predicted_score':pred_scores, 'true_score':true_scores})
+    data.to_csv('test_results.csv')
